a769308792dbedc210528ec8befda86ef8e9e404
KelvinMvungi <kelvin.mvungi01@gmail.com>
2023-10-05 17:18:07 +0300

    Add a function to show answer for faq 1

---
+++ faq-accordion-card/index.js
@@ -0,0 +1,7 @@
+
+function showAnswer1 () {
+    const dropdownText1 = document.getElementById("dropdown1");
+    const btnImg1 = document.getElementById('btn-img1');
+    dropdownText1.style.display = 'block';
+    btnImg1.style.transform = 'rotate(180deg)';
+}
