d43d7d139d10a7c5b7b123a6818019c708bda7b5
KelvinMvungi <kelvin.mvungi01@gmail.com>
2023-10-06 16:52:07 +0300

    Edit the showAnswer function for question 2

--- faq-accordion-card/index.js
+++ faq-accordion-card/index.js
@@ -5,3 +5,12 @@ function showAnswer1 () {
     dropdownText1.style.display = 'block';
     btnImg1.style.transform = 'rotate(180deg)';
 }
+
+function showAnswer2 () {
+    const dropdownText2 = document.getElementById("dropdown2");
+    const btnImg2 = document.getElementById('btn-img2');
+    dropdownText2.style.display = 'block';
+    btnImg2.style.transform = 'rotate(180deg)';
+}
+
+
