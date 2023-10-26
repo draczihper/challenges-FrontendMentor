f0321d3c1c926d0cb79acfe2f45d319e8ab5765b
KelvinMvungi <kelvin.mvungi01@gmail.com>
2023-10-26 09:51:38 +0300

    Fix not hiding the dropdown answer

--- faq-accordion-card/index.js
+++ faq-accordion-card/index.js
@@ -1,15 +1,16 @@
 
-function showAnswer() {
-    const dropdownText = document.querySelector(".dropdown");
-    const btnImg = document.querySelector('.btn-img');
-        dropdownText.style.display = 'block';
-        btnImg.style.transform = 'rotate(180deg)';
-}
+// function showAnswer() {
+   
+//     const btnImg = document.querySelector('.btn-img');
+//         dropdownText.style.display = 'block';
+//         btnImg.style.transform = 'rotate(180deg)';
+// }
 
+const dropdownText = document.querySelector(".dropdown");
 const questions = document.querySelectorAll(".question");
 
 questions.forEach((question) => {
-    question.addEventListener('click', () => {
-        showAnswer();
+    question.addEventListener('click', () => { 
+        dropdownText.style.display = (dropdownText.style.display === 'block') ? 'none' : 'block';
     });
 });
