17b3c94d7cde94d53f4dc558ad0a44c4e654a7b6
KelvinMvungi <kelvin.mvungi01@gmail.com>
2023-12-29 07:33:04 +0300

    Fix an attribute class name

--- article-preview-component/script.js
+++ article-preview-component/script.js
@@ -1,5 +1,5 @@
 const shareEl = document.querySelectorAll("#share");
 
-shareEl.addEventListener('click', () => {
-    console.log("Share icon is clicked!")
+shareEl.addEventListener('click', (e) => {
+    console.log("Share icon is clicked!");
 });
