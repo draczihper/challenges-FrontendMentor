2efad076e948d3dd1b197ba9fb20aff921c94abe
KelvinMvungi <kelvin.mvungi01@gmail.com>
2023-12-31 17:24:34 +0300

    Add a share div dom

--- article-preview-component/script.js
+++ article-preview-component/script.js
@@ -1,7 +1,9 @@
 const shareEl = document.getElementById("arrow");
+const shareDiv = document.getElementById("share-icons");
 
 shareEl.addEventListener("click", myFunction);
 
 function myFunction () {
+    
     console.log("Share Element clicked!");
 }
